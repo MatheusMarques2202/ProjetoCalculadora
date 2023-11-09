5508259b0ed2a9c0266af2c75fbb6fadccdd0838
Gspaiva57004 <gsantanapaiva123@gmail.com>
2023-11-09 08:23:28 -0300

    3

--- app/src/main/java/com/example/projetocalculadora/MainActivity.kt
+++ app/src/main/java/com/example/projetocalculadora/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.projetocalculadora
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -23,7 +28,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,7 +43,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ProjetoCalculadoraTheme {
-
+                AppCalculadorDeGorjeta()
                 }
             }
         }
@@ -46,74 +53,116 @@ class MainActivity : ComponentActivity() {
 @Preview(showSystemUi = true)
 @Composable
 
-fun calcularGorjeta(){
- var valorTotal by remember { mutableStateOf("") }
+fun AppCalculadorDeGorjeta() {
+    var totalConta by remember { mutableStateOf("") }
     var porcentagemGorjeta by remember { mutableStateOf("") }
-    val valorGorjeta = calculoDaGorjeta(
-        valorTotal.toDoubleOrNull()?: 0.0,
-        porcentagemGorjeta.toDoubleOrNull()?: 0.0,
+    var arredondar by remember { mutableStateOf(false) }
+    val valorGorjeta = gorjetaCalculo(
+        totalConta.toDoubleOrNull() ?: 0.0,
+        porcentagemGorjeta.toDoubleOrNull() ?: 0.0,
+        arredondar
     )
 
     Column(
-       verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize(),
-    )
-
-    {
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
         Text(
-            text = stringResource(R.string.Gorjeta)
+            text = "Calculador de Gorjeta"
+
         )
-       TextField(
-           value = valorTotal ,
-           onValueChange = {valorTotal = it},
-            label = {
-                Text(
-                    text = "valor"
-                )
+        CampoDeTexto(
+            value = totalConta,
+            onValueChange = { totalConta = it },
+            idTexto = R.string.valorGorjeta,
+            imeAction = ImeAction.Next
+        )
 
-            },
-           isError = false,
-           shape = RectangleShape,
-           singleLine = true,
-           keyboardOptions = KeyboardOptions (keyboardType = KeyboardType.Number)
-
-       )
-        Spacer(modifier = Modifier.size(10.dp))
-
-
-        TextField(
+        CampoDeTexto(
             value = porcentagemGorjeta,
-            onValueChange = {porcentagemGorjeta = it},
-            label = {
-                Text(
-                    text = "% gorjeta "
-                )
-
-            },
-            isError = false,
-            shape = RectangleShape,
-            singleLine = true,
-            keyboardOptions = KeyboardOptions (keyboardType = KeyboardType.Number)
-
-
-
+            onValueChange = { porcentagemGorjeta = it },
+            idTexto = R.string.valorGorjeta,
+            imeAction = ImeAction.Done
         )
-        Spacer(modifier = Modifier.size(10.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
+
+        ) {
+            Text(
+                text = "Arredondar Gorjeta?"
+            )
+            Switch(
+                checked = arredondar,
+                onCheckedChange = { arredondar = it }
+            )
+        }
         Text(
-            text = stringResource(id = R.string.valorGorjeta, valorGorjeta)
+
+            text = stringResource(R.string.valorGorjeta, valorGorjeta)
         )
 
 
     }
-}
-fun calculoDaGorjeta(valorTotal:Double, porcentagemGorjeta:Double = 15.0):String{
 
-  val gorjeta =  porcentagemGorjeta/100 * (valorTotal)
+}
+
+
+
+
+
+
+fun gorjetaCalculo(
+    totalConta: Double,
+    porcentagemGorjeta: Double = 15.0,
+    arredondar: Boolean = false
+): String {
+    var gorjeta = porcentagemGorjeta / 100 * totalConta
+    if (arredondar) {
+        gorjeta = kotlin.math.ceil(gorjeta)
+    }
     return NumberFormat.getCurrencyInstance().format(gorjeta)
 
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun CampoDeTexto(
+    value: String,
+    onValueChange: (String) -> Unit,
+    idTexto: Int,
+    imeAction: ImeAction,
+    @DrawablesRes iconeCampoTexto: Int
+) {
+
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        leadingIcon = { Icon( painter = painterResource(id = iconeCampoTexto)) },
+        label = {
+            Text(text = stringResource(id = idTexto))
+        },
+        isError = false,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Decimal,
+            imeAction = imeAction
+
+        ),
+
+        )
+    Spacer(
+        modifier = Modifier.size(7.dp)
+    )
 
 }
+
+
 
 
 
